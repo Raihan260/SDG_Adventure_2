3d167aeea0c3d5cf78cd067df629597860225b62
Raihan260 <rayhanandika26@gmail.com>
2025-05-08 10:02:32 +0700

    Changes to be committed: 	modified:   lib/Home/big_quest.dart

--- lib/Home/big_quest.dart
+++ lib/Home/big_quest.dart
@@ -1,10 +1,23 @@
-// big_quest.dart
 import 'package:flutter/material.dart';
 import 'package:sdg_adventure_2/color.dart';
 import 'package:sdg_adventure_2/Home/Big_Quest/big_quest_banner.dart';
 
-class BigQuest extends StatelessWidget {
+class BigQuest extends StatefulWidget {
   const BigQuest({super.key});
+
+  @override
+  State<BigQuest> createState() => _BigQuestState();
+}
+
+class _BigQuestState extends State<BigQuest> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +35,42 @@ class BigQuest extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 8),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: buildBigQuestCard(context, bigQuestData[0]),
+        SizedBox(
+          height: 180,
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: bigQuestData.length > 3 ? 3 : bigQuestData.length,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: buildBigQuestCard(context, bigQuestData[index]),
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 12),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            bigQuestData.length > 3 ? 3 : bigQuestData.length,
+            (index) => AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              width: _currentPage == index ? 12 : 8,
+              height: _currentPage == index ? 12 : 8,
+              decoration: BoxDecoration(
+                color: _currentPage == index
+                    ? AppColor.orange
+                    : AppColor.mainGrey,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
         ),
       ],
     );
